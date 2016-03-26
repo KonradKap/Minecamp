488568f0fb33cebe07127a694a05562cb2415479
Konrad Kaproń <kkapron@elka.pw.edu.pl>
2016-03-26 16:06:36 +0100

    Moved some files around to -hopefully- make project Makefile friendly.

--- src/PathManager.hpp
+++ src/PathManager.hpp
@@ -20,37 +20,38 @@ const std::string PathManager<T>::get(const T arg) const
 {
 	return paths_.at(arg);
 }
-
+/*
 template<class T>
 std::string PathManager<T>::resources()
 {
+	//return ofFilePath::join("..", "resources");
 	return ofFilePath::join(ofFilePath::getCurrentWorkingDirectory(), "resources");
 }
-
+*/
 template<class T>
 std::string PathManager<T>::texture(const std::string& target)
 {
-	return ofFilePath::join(dir("textures"), target);
+	return ofFilePath::join("textures", target);
 }
 
 template<class T>
 std::string PathManager<T>::font(const std::string& target)
 {
-	return ofFilePath::join(dir("fonts"), target);
+	return ofFilePath::join("fonts", target);
 }
 
 template<class T>
 std::string PathManager<T>::block(const std::string& target)
 {
-	return ofFilePath::join(dir("blocks"), target);
+	return ofFilePath::join("blocks", target);
 }
-
+/*
 template<class T>
 std::string PathManager<T>::dir(const std::string& target)
 {
 	return ofFilePath::join(resources(), target);
 }
-
+*/
 template<class T>
 PathManager<T>::PathManager(const std::map<const T, const std::string>& map) :
 	paths_(map)
